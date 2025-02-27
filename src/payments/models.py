b148ceee2b9ff219040de6a06adbe432ff7103b6
dogober <dogoberrr@gmail.com>
2025-02-27 19:14:29 +0200

    create Payment model

--- src/payments/models.py
+++ src/payments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Is in the process"
+        PAID = "PAID", "Successfully paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", "Common payment"
+        FINE = "FINE", "Penalty"
+
+    status = models.CharField(
+        max_length=10, choices=Status.choices, default=Status.PENDING
+    )
+    type = models.CharField(max_length=10, choices=Type.choices, default=Type.PAYMENT)
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+    session_url = models.URLField()
+    session_id = models.IntegerField()
+    models.DecimalField(max_digits=5, decimal_places=2)
