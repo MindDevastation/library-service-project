478948396d493de1d1b33a8bb938435e43f26ee5
taraskhom <khomenkotaras1305@gmail.com>
2025-03-01 18:16:04 +0200

    add creation functionality to BorrowingViewSet

--- src/borrowings/views.py
+++ src/borrowings/views.py
@@ -2,11 +2,18 @@ from rest_framework import viewsets
 from rest_framework import mixins
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingDetailSerializer, BorrowingListSerializer
+from borrowings.serializers import (
+    BorrowingDetailSerializer,
+    BorrowingListSerializer,
+    BorrowingCreateSerializer,
+)
 
 
 class BorrowingViewSet(
-    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
 ):
     serializer_class = BorrowingListSerializer
     queryset = Borrowing.objects.select_related("user", "book").prefetch_related(
@@ -16,4 +23,6 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
         return BorrowingListSerializer
