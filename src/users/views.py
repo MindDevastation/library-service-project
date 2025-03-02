f20cb4a245eebb076fe802cced8ce0ec4e174ab7
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-02 10:51:41 +0200

    Updated views

--- src/users/views.py
+++ src/users/views.py
@@ -1,13 +1,13 @@
-from rest_framework import generics, viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework import generics, viewsets, status
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from rest_framework.exceptions import MethodNotAllowed
+
 from users.models import User
 from books.models import Book
 from books.serializers import BookSerializer
-from users.serializers import (
-    UserRegistrationSerializer,
-    UserProfileSerializer,
-)
+from users.serializers import UserRegistrationSerializer, UserProfileSerializer
 
 
 class RegisterUserView(generics.CreateAPIView):
@@ -19,7 +19,21 @@ class RegisterUserView(generics.CreateAPIView):
 class UserProfileView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_object(self):
         return self.request.user
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        return [IsAdminUser()]
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed("DELETE", detail="Delete is not accepted.")
