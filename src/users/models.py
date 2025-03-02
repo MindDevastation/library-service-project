a9998c13316c664024e43de69b34cc3c36c5e9d3
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-02 18:37:54 +0200

    Add permissions and organize code

--- src/users/models.py
+++ src/users/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True, verbose_name="Email")
     first_name = models.CharField(max_length=60, verbose_name="Name")
     last_name = models.CharField(max_length=60, verbose_name="Surname")
-    is_staff = models.BooleanField(default=False, verbose_name="Is staff")
+    is_staff = models.BooleanField(default=True, verbose_name="Is staff")
     is_active = models.BooleanField(default=True, verbose_name="Active user")
     date_joined = models.DateTimeField(
         auto_now_add=True, verbose_name="Registration date"
