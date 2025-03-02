4452cbb2f4239545eecf107ae537a2b3d20a8263
taraskhom <khomenkotaras1305@gmail.com>
2025-03-02 10:11:41 +0200

    add filtering for borrowing viewset

--- src/borrowings/views.py
+++ src/borrowings/views.py
@@ -26,3 +26,30 @@ class BorrowingViewSet(
         if self.action == "create":
             return BorrowingCreateSerializer
         return BorrowingListSerializer
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.select_related("user", "book").prefetch_related(
+            "book__authors"
+        )
+
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(
+                    status__in=[Borrowing.Status.PENDING, Borrowing.Status.OVERDUE]
+                )
+            if is_active.lower() == "false":
+                queryset = queryset.filter(status__in=[Borrowing.Status.RETURNED])
+
+        if (
+            user_id
+            and self.request.user.is_superuser
+            or user_id
+            and user_id == str(self.request.user.id)
+        ):
+            if user_id.isdigit():
+                queryset = queryset.filter(user_id=user_id)
+
+        return queryset
