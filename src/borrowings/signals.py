764f460a61d640664ebfb3f5cb4eb91601816e43
Ruslan <ruslanyemets@gmail.com>
2025-03-02 08:44:12 +0200

    Add send_borrowing_confirmation_email method to signals

---
+++ src/borrowings/signals.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+from borrowings.models import Borrowing
+
+
+@receiver(post_save, sender=Borrowing)
+def send_borrowing_confirmation_email(sender, instance, created, **kwargs):
+    if created:
+        subject = f"Borrowing #{instance.id} Confirmation"
+        html_message = render_to_string(
+            "borrowing_confirmation_email.html", {"borrowing": instance}
+        )
+        plain_message = strip_tags(html_message)
+        from_email = "no-reply@yourdomain.com"
+        to_email = instance.user.email
+
+        send_mail(
+            subject,
+            plain_message,
+            from_email,
+            [to_email],
+            html_message=html_message,
+            fail_silently=True,
+        )
