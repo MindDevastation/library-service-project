081e9a44ecec0f2328bf3d7e7e86a59afb33d2c2
Aleksey Pastukhov <42119796+MindDevastation@users.noreply.github.com>
2025-03-03 15:03:28 +0200

    Fixed users/view.py

--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from users.models import User
-from users.serializers import UserRegistrationSerializer, UserProfileSerializer
-
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from users.serializers import UserSerializer
@@ -12,6 +8,7 @@ from users.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
