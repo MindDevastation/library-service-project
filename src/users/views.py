779a0dc66390514b7e92ba082fe38830372d23f2
Aleksey Pastukhov <42119796+MindDevastation@users.noreply.github.com>
2025-03-03 14:58:46 +0200

    Moves permission to borriwings/permitions.py

--- src/users/views.py
+++ src/users/views.py
@@ -2,15 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from users.models import User
 from users.serializers import UserRegistrationSerializer, UserProfileSerializer
-from rest_framework.permissions import BasePermission
 
-
-class IsBorrowingOwnerOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_staff or request.user == obj.user
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
