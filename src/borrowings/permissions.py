e130b65084b4623301b303eb36ddf809ddfeb5da
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-03 20:12:35 +0200

    Created permission for work with books

--- src/borrowings/permissions.py
+++ src/borrowings/permissions.py
@@ -23,3 +23,10 @@ class IsSelfOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj == request.user or (request.user and request.user.is_staff)
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
