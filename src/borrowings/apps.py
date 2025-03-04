363ef061dc6d1b979d5ddd3763e9f32606ea5cbe
Oleksandr Tsyhanok <shrek1337@icloud.com>
2025-03-04 08:03:34 +0200

    fixed AppsNotReadyError

--- src/borrowings/apps.py
+++ src/borrowings/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from django.utils.timezone import now
-from borrowings.tasks import check_and_update_overdue_borrowings
 
 class BorrowingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -10,9 +10,11 @@ class BorrowingsConfig(AppConfig):
     def ready(self):
         import borrowings.signals
 
-        self.create_periodic_task()
+        post_migrate.connect(self.create_periodic_task, sender=self)
 
     def create_periodic_task(self):
+
+        from borrowings.tasks import check_and_update_overdue_borrowings
 
         schedule, created = IntervalSchedule.objects.get_or_create(
             every=1,
